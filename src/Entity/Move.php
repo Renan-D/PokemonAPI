c0526b7b7fb2615309f3c20a8323ab3ec3013453
Renan Declercq <renan.declercq.etu@univ-lille.fr>
2024-06-08 20:58:27 +0200

    :sparkles: add Move entity

---
+++ src/Entity/Move.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MoveRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MoveRepository::class)]
+class Move
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $number = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $classification = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $category = null;
+
+    #[ORM\Column]
+    private ?int $power = null;
+
+    #[ORM\Column]
+    private ?int $accuracy = null;
+
+    #[ORM\Column]
+    private ?int $maxPP = null;
+
+    #[ORM\Column]
+    private ?int $currentPP = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $effect = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): static
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getClassification(): ?string
+    {
+        return $this->classification;
+    }
+
+    public function setClassification(string $classification): static
+    {
+        $this->classification = $classification;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?string $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getPower(): ?int
+    {
+        return $this->power;
+    }
+
+    public function setPower(int $power): static
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    public function getAccuracy(): ?int
+    {
+        return $this->accuracy;
+    }
+
+    public function setAccuracy(int $accuracy): static
+    {
+        $this->accuracy = $accuracy;
+
+        return $this;
+    }
+
+    public function getMaxPP(): ?int
+    {
+        return $this->maxPP;
+    }
+
+    public function setMaxPP(int $pp): static
+    {
+        $this->maxPP = $pp;
+
+        return $this;
+    }
+
+    public function getCurrentPP(): ?int
+    {
+        return $this->currentPP;
+    }
+
+    public function setCurrentPP(int $currentPP): static
+    {
+        $this->currentPP = $currentPP;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEffect(): ?string
+    {
+        return $this->effect;
+    }
+
+    public function setEffect(?string $effect): static
+    {
+        $this->effect = $effect;
+
+        return $this;
+    }
+}
